740f91fdb6d3a6de2d94e0cc318c52c2aee896f4
Dasradhvarma <32213936+Dasradhvarma@users.noreply.github.com>
2018-10-03 13:59:04 -0400

    Coding Assignment-1

---
+++ src/com/coding/assignment/dasradh/input/InputHandler.java
@@ -0,0 +1,12 @@
+package com.coding.assignment.dasradh.input;
+
+/**
+ * Created on 12/4/15, 4:02 PM
+ * InputHandler.java
+ *
+ * @author gshankar
+ */
+public interface InputHandler {
+
+    public void process() throws Exception;
+}
